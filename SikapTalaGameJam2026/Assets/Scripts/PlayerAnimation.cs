90561159b47caa662bc1376f9cfcf2bad6b729f8
RavenFraust12 <tjvangeles08@gmail.com>
2026-05-12 00:52:00 +0800

    update 05/12/2026 - 12:52am
    - added animation to player
    TRUST

---
+++ SikapTalaGameJam2026/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void PlayIdleAnim()
+    {
+        PlayAnimation(0);
+    }
+
+    public void PlayJogAnim()
+    {
+        PlayAnimation(1);
+    }
+
+    public void PlayJumpAnim()
+    {
+        PlayAnimation(2);
+    }
+
+    public void PlayFallAnim()
+    {
+        PlayAnimation(3);
+    }
+
+    public void PlayLandingAnim()
+    {
+        PlayAnimation(4);
+    }
+
+    void PlayAnimation(int index)
+    {
+        /*if (IsPlaying(animName))
+            return;*/
+
+        anim.SetInteger("PlayerAnimation", index);
+    }
+
+    bool IsPlaying(string animName)
+    {
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        return stateInfo.IsName(animName);
+    }
+}
--- SikapTalaGameJam2026/Assets/Scripts/PlayerMovement.cs
+++ SikapTalaGameJam2026/Assets/Scripts/PlayerMovement.cs
@@ -2,44 +2,134 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 5f;
 
+    [Header("Jump")]
     public float jumpHeight = 2f;
     public float gravity = -9.81f;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
     public float groundedForce = -2f;
 
-    CharacterController controller;
-    float verticalVelocity;
+    private CharacterController controller;
+    private PlayerAnimation anim;
+
+    private float verticalVelocity;
+
+    private bool wasGrounded;
+    private bool isFalling;
+    private bool isJumping;
+    private bool isLanding;
 
     void Awake()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<PlayerAnimation>();
     }
 
     public void Move()
     {
-        if (controller.isGrounded && verticalVelocity < 0f)
-            verticalVelocity = groundedForce;
+        bool grounded = controller.isGrounded;
 
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = (transform.right * x + transform.forward * z).normalized;
+        Vector3 move =
+            (transform.right * x + transform.forward * z).normalized;
+
+        bool isMoving = move.magnitude > 0.1f;
+
+        // Horizontal movement
         move *= moveSpeed;
 
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
-            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        // Landing detection
+        if (grounded && !wasGrounded && isFalling)
+        {
+            if (!isMoving)
+            {
+                anim.PlayLandingAnim();
+                isLanding = true;
 
+                Invoke(nameof(EndLanding), 0.3f);
+            }
+
+            isFalling = false;
+        }
+
+        if (grounded && verticalVelocity < 0f)
+        {
+            verticalVelocity = groundedForce;
+        }
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            Jump();
+        }
+
+        // Better jump physics
         if (verticalVelocity < 0f)
-            verticalVelocity += gravity * (fallMultiplier - 1f) * Time.deltaTime;
-        else if (verticalVelocity > 0f && !Input.GetButton("Jump"))
-            verticalVelocity += gravity * (lowJumpMultiplier - 1f) * Time.deltaTime;
+        {
+            if (!grounded)
+            {
+                Fall();
+            }
+
+            verticalVelocity +=
+                gravity * (fallMultiplier - 1f) * Time.deltaTime;
+        }
+        else if (verticalVelocity > 0f &&
+                 !Input.GetButton("Jump"))
+        {
+            verticalVelocity +=
+                gravity * (lowJumpMultiplier - 1f) * Time.deltaTime;
+        }
 
         verticalVelocity += gravity * Time.deltaTime;
 
         move.y = verticalVelocity;
+
         controller.Move(move * Time.deltaTime);
+
+        // Ground animations
+        if (grounded && !isFalling && !isJumping && !isLanding)
+        {
+            if (isMoving)
+            {
+                anim.PlayJogAnim();
+            }
+            else
+            {
+                anim.PlayIdleAnim();
+            }
+        }
+
+        wasGrounded = grounded;
+    }
+
+    void Jump()
+    {
+        anim.PlayJumpAnim();
+
+        isJumping = true;
+
+        verticalVelocity =
+            Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    void Fall()
+    {
+        if (isFalling)
+            return;
+
+        anim.PlayFallAnim();
+        isJumping = false;
+        isFalling = true;
+    }
+
+    void EndLanding()
+    {
+        isLanding = false;
     }
 }
