c7bef5862e05529b28ae0e4a6775748d11959ba2
RavenFraust12 <tjvangeles08@gmail.com>
2026-05-13 04:04:19 +0800

    update 05/13/2026 - 4:05am
    - fixed 3D spatial of wall colliders
    - fixed drawer height
    - wall colliders have different intervals

--- SikapTalaGameJam2026/Assets/Scripts/Obstacle Script/WallColliderObstacle.cs
+++ SikapTalaGameJam2026/Assets/Scripts/Obstacle Script/WallColliderObstacle.cs
@@ -3,15 +3,29 @@ using UnityEngine;
 public class WallColliderObstacle : MonoBehaviour
 {
     AudioSource src;
+    Animator anim;
+    public float animInterval;
 
     private void Awake()
     {
         src = GetComponent<AudioSource>();
-
         if (src == null) src = gameObject.AddComponent<AudioSource>();
+
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        Invoke("StartAnimation", animInterval);
+    }
+
+    public void StartAnimation()
+    {
+        anim.Play("New Closing Walls Anim");
     }
     public void PlayWallColliderSFX()
     {
+        Debug.Log($"Played in {src.gameObject.name}");
         AudioManager.instance.PlayWallColliderSFX(src);
     }
 }
