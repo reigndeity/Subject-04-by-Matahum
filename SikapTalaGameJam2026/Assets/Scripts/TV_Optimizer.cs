bc81ac79597e00ea52a6843a5dbad1095512e551
RavenFraust12 <tjvangeles08@gmail.com>
2026-05-13 04:26:33 +0800

    Update 05/13/2026 - 4:28am
    - added "optimization" to TVs

---
+++ SikapTalaGameJam2026/Assets/Scripts/TV_Optimizer.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class TV_Optimizer : MonoBehaviour
+{
+    private Player player;
+
+    [Header("Cameras")]
+    public Camera frontCamera;
+    public Camera backCamera;
+
+    [Header("Optimization")]
+    public float distanceToRender = 15f;
+
+    private bool camerasActive;
+
+    private void Awake()
+    {
+        player = FindFirstObjectByType<Player>();
+    }
+
+    private void Start()
+    {
+        SetCameras(false);
+    }
+
+    private void Update()
+    {
+        if (player == null)
+            return;
+
+        float distance =
+            Vector3.Distance(
+                transform.position,
+                player.transform.position
+            );
+
+        bool shouldRender = distance <= distanceToRender;
+
+        if (shouldRender != camerasActive)
+        {
+            SetCameras(shouldRender);
+        }
+    }
+
+    void SetCameras(bool state)
+    {
+        camerasActive = state;
+
+        if (frontCamera != null)
+            frontCamera.enabled = state;
+
+        if (backCamera != null)
+            backCamera.enabled = state;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawWireSphere(
+            transform.position,
+            distanceToRender
+        );
+    }
+}
