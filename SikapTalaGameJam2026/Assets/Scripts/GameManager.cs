e35185de441fd7bf38248153e8cce92315e89529
Kyrsten Shaen  Sasil <142300998+zenjlle@users.noreply.github.com>
2026-05-12 17:59:56 +0800

    update 5/12/2026 - 5:59pm
    - setting up intro cutscene

--- SikapTalaGameJam2026/Assets/Scripts/GameManager.cs
+++ SikapTalaGameJam2026/Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     [Header("Cutscenes")]
     public PlayableDirector playableDirector;
     public PlayableAsset cutscene1;
+    public PlayableAsset cutscene2;
 
     [Header("Tutorials")]
     public GameObject movementTutorial;
@@ -22,6 +23,11 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             StartCoroutine(StartGame());
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            playableDirector.playableAsset = cutscene2;
+            playableDirector.Play();
         }
     }
     IEnumerator StartGame()
@@ -42,5 +48,34 @@ public class GameManager : MonoBehaviour
         });
 
         movementTutorial.SetActive(true);
+    }
+
+    public void CCTVTutorial(int current)
+    {
+        playableDirector.Pause();
+        StartCoroutine(DoingCCTVTutorial(current));
+    }
+    IEnumerator DoingCCTVTutorial(int current) 
+    {
+        switch (current) 
+        {
+            case 0:
+                yield return DialogueManager.instance.PlayDialogue(new string[]
+                {
+                    "Interesting...",
+                    "It seems I can control the CCTV using the <color=yellow>WASD</color> keys"
+                });
+                playableDirector.Resume();
+                break;
+            case 1:
+                yield return DialogueManager.instance.PlayDialogue(new string[]
+                {
+                    "Huh?",
+                    "What's that?!"
+                });
+                playableDirector.Resume();
+                break;
+        }
+
     }
 }
